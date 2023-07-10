eb8f62cb4e337cdf0f42515129820d73a54641fb
liuxj-j <liuxj-j@glodon.com>
2023-07-10 11:16:55 +0800

    [feature]: 万里长征第一步☝️

---
+++ jucaculators/jucaculatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  jucaculatorsApp.swift
+//  jucaculators
+//
+//  Created by glodon on 2023/7/10.
+//
+
+import SwiftUI
+
+@main
+struct jucaculatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
